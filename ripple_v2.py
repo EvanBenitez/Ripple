f7c6030e7af57c24043c35f1545cbeb4c738dc03
Nick Cleary <nicholascleary01@gmail.com>
2022-04-14 16:27:09 -0400

    Updates to v2

--- ripple_v2.py
+++ ripple_v2.py
@@ -8,6 +8,8 @@ from threading import Thread, Event
 from typing import Tuple
 
 MAX_VALUE = 128
+HIGHEST_INCREASE = 32
+RADIUS_FACTOR = 2
 
 event_var = Event()
 
@@ -28,19 +30,61 @@ def gray(im):
 
 class Circle:
     def __init__(self, center: Tuple[int, int]):
-        self.center = (math.floor(center[0]/3), math.floor(center[1]/3))
-        self.radius = 0
+        self.center = (math.floor(center[0] / 3), math.floor(center[1] / 3))
+        self.center_x = math.floor(center[1] / 3)
+        self.center_y = math.floor(center[0] / 3)
+        self.current_radius = 1
         self.active = True
+
+        self.max_radius = max(math.sqrt(self.center[0] ** 2 + self.center[1] ** 2),
+                              math.sqrt((300 - self.center[0]) ** 2 + self.center[1] ** 2),
+                              math.sqrt(self.center[0] ** 2 + (300 - self.center[1]) ** 2),
+                              math.sqrt((300 - self.center[0]) ** 2 + (300 - self.center[1]) ** 2)
+                              )/RADIUS_FACTOR
+
+    def __repr__(self):
+        return f'Center: {self.center}. Radius: {self.current_radius}. Active: {self.active}'
 
 
 circles: [Circle] = []
+x = np.arange(0, 300)
+y = np.arange(0, 300)
+
+
+def draw_circle(circle: Circle, internal_array):
+    for i in range(8):
+        radius = circle.current_radius - i
+        if radius <= 0:
+            break
+        mask = np.power((x[np.newaxis, :] - circle.center_x), 2) + np.power((
+                    y[:, np.newaxis] - circle.center_y), 2) < np.power((radius * RADIUS_FACTOR), 2)
+        internal_array[mask] += (HIGHEST_INCREASE - (4 * i))
+    circle.current_radius += 1
 
 
 def calculate():
+    previous_circle_string = str(circles)
     while not event_var.is_set():
+        # If the circles array has not changed since previous iteration, wait and continue
+        if str(circles) == previous_circle_string:
+            time.sleep(0.5)
+            continue
+
+        previous_circle_string = str(circles)
+        print(previous_circle_string)
+        internal_array = np.zeros((300, 300), dtype=np.int8)
         for circle in circles:
-            center = circle.center
-            array[center[0]][center[1]] = 100
+            # If a circle has completed its animation, get rid of it
+            if circle.current_radius >= circle.max_radius:
+                circles.remove(circle)
+                continue
+
+            draw_circle(circle, internal_array)
+
+        # Update Display
+        surface = pygame.surfarray.make_surface(gray(internal_array))
+        display.blit(surface, (0, 0))
+        pygame.display.update()
 
 
 def startup():
@@ -51,10 +95,11 @@ def startup():
 
 
 display, array = startup()
+running = True
+is_updating = False
 calculator_thread = Thread(target=calculate)
 calculator_thread.start()
 surface = pygame.surfarray.make_surface(gray(array))
-running = True
 
 while running:
     for event in pygame.event.get():
@@ -64,7 +109,4 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
             circles.append(Circle((mouse_pos[0], mouse_pos[1])))
-    display.blit(surface, (0, 0))
-    surface = pygame.surfarray.make_surface(gray(array))
-    pygame.display.update()
 pygame.quit()
